030126ad4805879a0ce23c61521564cb4f5d4025
Leandro-Reolon <leandrohreolon@gmail.com>
2025-07-29 21:14:37 -0300

    v1.5

--- Home/urls.py
+++ Home/urls.py
@@ -4,3 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),  # Include the URLs from the tarefas app
 ]
+
+
--- RotaTocantinense/urls.py
+++ RotaTocantinense/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+
+
 
 urlpatterns = [
     path('', include('Home.urls')),  # Include the URLs from the tarefas app
+]
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),          # <-- Adicione essa linha
+    path('', include('Home.urls')),           # Rota do seu app principal
 ]
