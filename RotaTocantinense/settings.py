2c64c23913313afb19f9a68c148147e567b05f7d
Leandro-Reolon <leandrohreolon@gmail.com>
2025-08-10 23:42:52 -0300

    v9.8

--- RotaTocantinense/settings.py
+++ RotaTocantinense/settings.py
@@ -75,13 +75,20 @@ WSGI_APPLICATION = 'RotaTocantinense.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/data', 'db.sqlite3'), # indicar onde fica o banco de dados
+if os.environ.get('RAILWAY_ENVIRONMENT'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/data/db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
